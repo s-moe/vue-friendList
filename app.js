f8238ba6f927d053e55f77b814b644e59c3360c0
s-moe <sarahannmoe@gmail.com>
2021-10-27 06:51:55 -0700

    friend-contact component made

--- app.js
+++ app.js
@@ -18,4 +18,33 @@ const app = Vue.createApp({
     };
   },
 });
+app.component("friend-contact", {
+  template: `
+    <li>
+    <h2>{{friend.name}}</h2>
+    <button @click="toggleDetails()">
+      {{ detailsAreVisible ? 'Hide' : 'Show Details' }}
+    </button>
+    <ul v-if="detailsAreVisible">
+      <li><strong>Phone:</strong>{{friend.phone}}</li>
+      <li><strong>Email:</strong> {{friend.email}}</li>
+    
+    `,
+  data() {
+    return {
+      detailsAreVisible: false,
+      friend: {
+        id: "manuel",
+        name: "Manuel Lorenz",
+        phone: "01234567",
+        email: "manuel@manuel.com",
+      },
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    },
+  },
+});
 app.mount("#app");
